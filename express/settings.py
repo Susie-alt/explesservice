ecc6f501abc725e83adda0558f9eaca9c1ee1893
Susie-alt <suubonnee@gmail.com>
2024-11-01 23:52:12 -0700

    Update settings.py

--- express/settings.py
+++ express/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0xjxktsurztxh%i!4r6=&!t7kx^+ya9h=%2ycwj%%%$dbn(5ur'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['https://mysite-ksfn.onrender.com/']
 
 
 # Application definition
